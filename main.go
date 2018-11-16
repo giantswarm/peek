74f05acd546ef06509ee0616fd82623eed6f83cf
Tuomas Makinen <tuommaki@gmail.com>
2018-11-16 13:59:20 +0200

    Initial implementation of peek
    Peek is a helper program that provides HTTP server listening in
    /callback for requests. If url parameter is defined, it will call that
    and pipe response back to original caller. Otherwise simple text
    response is returned. /healthz provides liveness/readiness check for k8s
    deployments.
    
    This tool can be used to create long chains of service-pod-servce
    deployment chains in order to test e.g. multi-az deployments or other
    networking related issues.

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+var listenAddr = flag.String("listen", ":8080", "Listen address. Default: :8080")
+var next = flag.String("url", "", "URL to call when handling a request.")
+
+func main() {
+	flag.Parse()
+
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "All good®")
+	})
+
+	http.HandleFunc("/callme", func(w http.ResponseWriter, r *http.Request) {
+		if next == nil || *next == "" {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "You reached the end of the chain.")
+			return
+		}
+
+		log.Println("Calling next server ", *next)
+
+		resp, err := http.Get(*next)
+		if err != nil {
+			log.Printf("http.Get(%s) failed: %#q\n", *next, err)
+			resp.Body.Close()
+			w.WriteHeader(http.StatusBadGateway)
+			fmt.Fprintf(w, "%#q", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		w.WriteHeader(http.StatusOK)
+		p := make([]byte, 4096)
+		for {
+			nRead, err := resp.Body.Read(p)
+			if nRead <= 0 && err == io.EOF {
+				break
+			}
+
+			nWritten, err := w.Write(p[:nRead])
+			if err != nil {
+				log.Printf("error while writing response: %#q\n", err)
+				break
+			}
+
+			if nWritten < nRead {
+				// Try once.
+				nWritten, err = w.Write(p[nWritten:nRead])
+				if err != nil {
+					log.Printf("error while writing response: %#q\n", err)
+					break
+				}
+			}
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(*listenAddr, nil))
+}
